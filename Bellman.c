1015c879ec0e377e4e09ab18a7a1fe04f42891e5
Zelantine <antonin.macquart.15@gmail.com>
2016-05-18 16:27:08 +0200

    Create Bellman.c

---
+++ Bellman.c
@@ -0,0 +1,7 @@
+void over_9000(GRAPHE G){ //fonction qui met les poids des sommets à +inf (ou valeur très grande)
+  SOMMET* tab_s = G->sommet;
+	int i=0;
+	for (i=0;i=(G->nX);i++){
+		(G->sommet)[i]->pds = 1 000 000;
+		}
+	}
