dd22226d91e8b815d5681aca3684325c18d2eb22
Zelantine <antonin.macquart.15@gmail.com>
2016-05-18 17:30:08 +0200

    Update Bellman.c

--- Bellman.c
+++ Bellman.c
@@ -1,7 +1,17 @@
 void over_9000(GRAPHE G){ //fonction qui met les poids des sommets à +inf (ou valeur très grande)
-  SOMMET* tab_s = G->sommet;
+  	SOMMET* tab_s = G->sommet;
 	int i=0;
 	for (i=0;i=(G->nX);i++){
 		(G->sommet)[i]->pds = 1 000 000;
-		}
+		
 	}
+}
+
+double lecture_poids(SOMMET* S){
+	return(*S->pds)
+	
+ARC* fetch_arc(SOMMET*Dep, SOMMET*Arr){ //fonction qui retourne l'adresse de l'arc entre les sommets Dep et Arr, et qui renvoie un erreur si il n'existe pas un unique arc qui les relie
+	int i=0;
+	if (!est_vide(Dep->PremArc)){
+		while	// test existence de l'arc suivant
+}
