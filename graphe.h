180809fe36370534c9e4d9408c5df84fcc05dc3f
Ekzow <t.chedal@gmail.com>
2016-05-25 16:30:45 +0200

    Update graphe.h

--- graphe.h
+++ graphe.h
@@ -1,4 +1,3 @@
-#include "LARC.h"
 #include "stdio.h"
 #include "stdlist.h"
 
@@ -8,7 +7,7 @@ typedef struct
 	SOMMET* dep;  //Adresse du sommet de départ dans le tableau des sommets
 	SOMMET* arr;  //Adresse "    "    d'arrivée "     "    "     "     "
 	double pds;  // Poids de l'arc
-} ARC
+} ARC;
 
 typedef struct 
 {
@@ -18,9 +17,9 @@ typedef struct
 	char ligne;   //Ligne sur laquelle est le sommet
 	char nom;   //Nom du sommet
 	double pds;   //Poids du sommet
-	LARC PremArc;  //Premier arc partant de ce sommet
+	LARC* PremArc;  //Premier arc partant de ce sommet
 	unsigned int deg=0;   //Degré du sommet (0 par défault)
-} SOMMET
+} SOMMET;
 
 typedef struct 
 {
@@ -28,18 +27,10 @@ typedef struct
 	ARC* arcs;
 	unsigned int nX;   //Nombre de sommet
 	unsigned int nA;   // Nombre d'arcs
-} GRAPHE
+} *GRAPHE;
 
-graphe nouveau_graphe(unsigned int nX,unsigned int nA)
-{
-	GRAPHE g;
-	g.nX=nX;
-	g.nA=nA;
-	g.sommet=calloc(nX,sizeof(SOMMET));   //Allocation du tableau des sommets
-	g.arc=calloc(nA,sizeof(ARC));  //Allocation du tableau des arcs
-	return g;
-}
+//graphe nouveau_graphe(unsigned int nX,unsigned int nA)
 
-void set_deg_nul(GRAPHE g)
+//void set_deg_nul(GRAPHE g)
 
-GRAPHE construire_graphe() 
+//GRAPHE construire_graphe() 
