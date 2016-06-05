9758a683e3d593a5a600d6f7fecafb3ab52fcd74
Ekzow <t.chedal@gmail.com>
2016-06-05 19:05:04 +0200

    Version finale

--- graphe.c
+++ graphe.c
@@ -6,7 +6,7 @@ void afficher_sommet(SOMMET s) //Affiche les noms et ligne d'un sommet
 	printf("Ligne : %s\n", s.ligne);
 }
 
-GRAPHE nouveau_graphe()
+GRAPHE nouveau_graphe() //Création et initialisation d'un graphe
 {
 	GRAPHE g;
 	g->Graphe=NULL;
@@ -15,7 +15,7 @@ GRAPHE nouveau_graphe()
 	return g;
 }
 
-GRAPHE construire_graphe(char* NomFichier, GRAPHE g) 
+GRAPHE construire_graphe(char* NomFichier, GRAPHE g)  //Remplissage du graphe
 {
 	FILE* f;
 	unsigned int nX; unsigned int nA; int i;
@@ -26,23 +26,23 @@ GRAPHE construire_graphe(char* NomFichier, GRAPHE g)
 	ARC a;
 	
 	
-	f=fopen("graphe1.csv","r");
+	f=fopen("graphe1.csv","r"); //A remplacer par nomfichier, je testais ici.
 	fscanf (f, "%d %d", &nX, &nA);
 	g=nouveau_graphe();
-	SOMMET st[nX];	
+	SOMMET st[nX];	//création d'un tableau de nX sommet, le calloc me faisait de la merde
 	
 	g->nX=nX;
 	printf("%d", nX);
 	g->nA=nA;
 	printf("%d", nA);
-	g->Graphe=st;
+	g->Graphe=st; //initialisation
 
 	fgets(ignore,sizeof(ignore),f);   //On ignore la ligne de texte inutile du .csv
 	
 	for(i=0;i<nX;i++)   //Boucle de remplissage du tableau des sommets
 	{
-		fscanf(f, "%d %lf %lf", &num, &lat, &lon);  //Indice, latitude, longitude, ligne
-		fgets(lig,256,f);
+		fscanf(f, "%d %lf %lf", &num, &lat, &lon);  //Indice, latitude, longitude
+		fgets(lig,256,f); //nom de la ligne
 		s.ind=num;
 		s.X=lat;
 		s.Y=lon;
@@ -55,16 +55,16 @@ GRAPHE construire_graphe(char* NomFichier, GRAPHE g)
 	
 	for(i=0;i<nA;i++)   //Boucle de remplissage du graphe
 	{
-		fscanf(f,"%d %d %lf", &depa, &arri, &pds);
+		fscanf(f,"%d %d %lf", &depa, &arri, &pds); //Départ, arrivée, poids
 		a.dep=depa;
 		a.arr=arri;
 		a.pds=pds;
-		g->Graphe[depa].PremArc=ajout_tete(a,g->Graphe[depa].PremArc);
+		g->Graphe[depa].PremArc=ajout_tete(a,g->Graphe[depa].PremArc); //Ajout de l'ar a la liste du bon sommet
 	}
 	return g;
 }
 
-void afficher_graphe(GRAPHE g)
+void afficher_graphe(GRAPHE g) //Petite fonction d'affichage, a optimiser selon la taille du fichier .csv
 {
 	int i;
 	for (i=0;i<(g->nX);i++)
