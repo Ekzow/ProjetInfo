2213d2d10ba28f99bed0466073cbf6048a648b36
Ekzow <t.chedal@gmail.com>
2016-06-05 16:18:43 +0200

    Update graphe.h

--- graphe.h
+++ graphe.h
@@ -1,47 +1,31 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include "LARC.h"
 
+typedef struct SOMMET SOMMET;
 
-typedef struct 
-{
-	unsigned int dep;  //Adresse du sommet de départ dans le tableau des sommets
-	unsigned int arr;  //Adresse "    "    d'arrivée "     "    "     "     "
-	double pds;  // Poids de l'arc
-} ARC;
-
-typedef struct MaillonLARC MaillonLARC;
-
-typedef MaillonLARC* LARC;
-
-struct MaillonLARC
-{
-	ARC arc;
-	MaillonLARC* suivant;
-};
-
-
-typedef struct 
+struct SOMMET
 {
 	unsigned int ind; //valeur du sommet
+	SOMMET* pere; //pere du sommet
 	double X;	//Latitude du sommet
 	double Y;  //Longitude du sommet
-	char ligne;   //Ligne sur laquelle est le sommet
-	char nom;   //Nom du sommet
+	char* ligne;   //Ligne sur laquelle est le sommet
+	char* nom;   //Nom du sommet
 	double pds;   //Poids du sommet
 	LARC PremArc;  //Premier arc partant de ce sommet
-	unsigned int deg;   //Degré du sommet (0 par défault)
-} SOMMET;
+	//unsigned int deg;   //Degré du sommet (0 par défault)
+};
 
 typedef struct 
 {
-	SOMMET* sommet;
-	ARC* arcs;
+	SOMMET* Graphe;
 	unsigned int nX;   //Nombre de sommet
 	unsigned int nA;   // Nombre d'arcs
-} *GRAPHE;
+}GRA;
 
-//graphe nouveau_graphe(unsigned int nX,unsigned int nA)
+typedef GRA* GRAPHE;
 
-//void set_deg_nul(GRAPHE g)
-
-//GRAPHE construire_graphe() 
+//FONCTION SUR LES GRAPHES
+void afficher_sommet(SOMMET s);
+GRAPHE nouveau_graphe();
+GRAPHE construire_graphe(char* NomFichier, GRAPHE g);
+void afficher_graphe(GRAPHE g);
