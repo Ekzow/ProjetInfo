899fa1778e68def35ddd014bf312263819e827a1
Ekzow <t.chedal@gmail.com>
2016-06-05 16:18:09 +0200

    Update graphe.c

--- graphe.c
+++ graphe.c
@@ -1,89 +1,75 @@
-#inculde "graphe.h"
+#include "graphe.h"
 
-
-void set_deg_nul(GRAPHE g)
+void afficher_sommet(SOMMET s) //Affiche les noms et ligne d'un sommet
 {
-	int i;
-	
-	for(i=0;i<(g.nX);i++)   //Pour tout les sommets
-	{
-		if (*(g.sommet+i)->deg)==0)   // Si leur degré est nul après construction
-		{
-			(*(g.sommet+i)->PremArc)=NULL;   //Réinitialisation de l'adresse
-		}
-	}
+	printf("Station : %s\n", s.nom);
+	printf("Ligne : %s\n", s.ligne);
 }
 
-GRAPHE construire_graphe() 
+GRAPHE nouveau_graphe()
+{
+	GRAPHE g;
+	g->Graphe=NULL;
+	g->nX=0;
+	g->nA=0;
+	return g;
+}
+
+GRAPHE construire_graphe(char* NomFichier, GRAPHE g) 
 {
 	FILE* f;
-	unsigned int nX; unsigned int nA; int i
-	GRAPHE g;
+	unsigned int nX; unsigned int nA; int i;
 	char ignore[1024];
-	unsigned int num; double lat; double lon; char[256] lig; char[512] sta;
+	unsigned int num; double lat; double lon; char lig[256]; char sta[512];
 	SOMMET s;
-	int N=0; unsigned int depa; unsigned int arri; double pds;
+	unsigned int depa; unsigned int arri; double pds;
 	ARC a;
 	
 	
-	f=fopen("graphe1.csv","r+");
+	f=fopen("graphe1.csv","r");
 	fscanf (f, "%d %d", &nX, &nA);
-	g=nouveau_graphe(nX,nA);   //Allocation mémoire initiale et création des variables
+	g=nouveau_graphe();
+	SOMMET st[nX];	
 	
+	g->nX=nX;
+	printf("%d", nX);
+	g->nA=nA;
+	printf("%d", nA);
+	g->Graphe=st;
+
 	fgets(ignore,sizeof(ignore),f);   //On ignore la ligne de texte inutile du .csv
 	
-	for(i=0,i<nX,i++)   //Boucle de remplissage du tableau des sommets
+	for(i=0;i<nX;i++)   //Boucle de remplissage du tableau des sommets
 	{
-		fscanf(f, "%d %lf %lf %s", &num, &lat, &lon, &lig)  //Indice, latitude, longitude, ligne
+		fscanf(f, "%d %lf %lf", &num, &lat, &lon);  //Indice, latitude, longitude, ligne
+		fgets(lig,256,f);
 		s.ind=num;
 		s.X=lat;
 		s.Y=lon;
-		s.ligne=nom;
+		s.ligne=lig;
 		fgets(sta,512,f);  //Nom de la station
 		s.nom=sta;
-		*(g.sommet+i)=s;
+		g->Graphe[i]=s;
 	}
-		
 	fgets(ignore,sizeof(ignore),f);   //On ignore la ligne de texte inutile
 	
-	*(g.sommet)->PremSom=g.arcs  //Initialisation du lien sommet-arc
-	
-	for(i=0,i<nA,i++)   //Boucle de remplissage du graphe
+	for(i=0;i<nA;i++)   //Boucle de remplissage du graphe
 	{
-		fscanf(f,"%d %d %lf", &depa, &arri, &pds)
-		if(dep==N)   //On gère l'affectation des adresse des sommets
-		{
-			a.dep=(g.sommet+depa);
-			a->arr=(g.sommet+arri);
-			a->pds=pds;
-			*(g.arcs+i)=a
-			*(g.sommet+depa)->deg+=1;
-		}
-		else
-		{
-			do
-			{
-				N+=1;   //Si il n'y a pas d'autres arcs qui partent du sommet, on passe au suivant dans le premier tableau(Rq: le while sert si le sommet suivant a un degré nul)
-				*(g.sommet+N)->PremArc=g.arcs+i
-			}
-			while(N!=dep)  
-			
-			a.dep=(g.sommet+depa);  //Et on répete le processus du if.
-			a->arr=(g.sommet+arri);
-			a->pds=pds;
-			*(g.arcs+i)=a
-			*(g.sommet+depa)->deg+=1;
-		
+		fscanf(f,"%d %d %lf", &depa, &arri, &pds);
+		a.dep=depa;
+		a.arr=arri;
+		a.pds=pds;
+		g->Graphe[depa].PremArc=ajout_tete(a,g->Graphe[depa].PremArc);
 	}
-set_deg_nul(g);   //On reset les adresses des degrés nul.
-
-return g
+	return g;
 }
 
-void affiche_graphe(GRAPHE g)
+void afficher_graphe(GRAPHE g)
 {
-	
+	int i;
+	for (i=0;i<(g->nX);i++)
+	{
+		afficher_sommet(g->Graphe[i]);
+		afficher_larc(g->Graphe[i].PremArc);
+	}
 }
-
-	
-	
