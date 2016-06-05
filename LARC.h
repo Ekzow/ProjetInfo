d84b1c0d1be98fd9165115ed9537352ff2bcc064
Ekzow <t.chedal@gmail.com>
2016-06-05 13:31:34 +0200

    Version finale

--- LARC.h
+++ LARC.h
@@ -1,14 +1,37 @@
 #include "stdio.h"
-#include "stdlist.h"
+#include "stdlib.h"
 
-typedef struct
+
+typedef struct 
 {
-	MaillonLARC* List
-} LARC
+	unsigned int dep;  //Adresse du sommet de départ dans le tableau des sommets
+	unsigned int arr;  //Adresse "    "    d'arrivée "     "    "     "     "
+	double pds;  // Poids de l'arc
+} ARC;
 
-typedef struct
+typedef struct MaillonLARC MaillonLARC;
+
+typedef MaillonLARC* LARC;
+
+struct MaillonLARC
 {
-	ARC arc
-	MaillonLARC* suivant
-} MaillonLARC
+	ARC arc;
+	MaillonLARC* suivant;
+};
 
+//FONCTIONS SUR LES LISTES
+LARC creer_LARC();
+int est_vide(LARC L);
+LARC ajout_tete(ARC a, LARC L);
+LARC supprimer_tete(LARC L);
+ARC renvoie_tete(LARC L);
+ARC pop_tete(LARC* L);
+ARC renvoi_arc(LARC L, int id);
+int test_egalite(ARC a,ARC b);
+LARC recherche_arc(LARC L, ARC a);
+void supprimer_arc(LARC* L, ARC a);
+int taille_larc(LARC L);
+void supprimer_indice(LARC* L, int id);
+LARC vide_larc(LARC L);
+void afficher_arc (ARC a);
+void afficher_larc(LARC L);
